b547ccd78e30ee81983b5e16e8234d3bb86fdfae
Sarandeep Kaur <kaurs4@students.rowan.edu>
2019-01-22 19:33:05 -0500

    I changed the name/email

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,8 @@
 
 
 /***********************************************
-* Name: Takeshwari Kamal
-* Email: kamalt4@students.rowan.edu
+* Name: Sarandeep Kaur
+* Email: kaurs4@students.rowan.edu
 ***********************************************
 * Purpose:
 *  This program calculates area of circle
