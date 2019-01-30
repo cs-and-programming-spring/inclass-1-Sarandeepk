1e4d82a53f8d182f9ddfc539d16c7970c1af46e3
Sarandeep Kaur <kaurs4@students.rowan.edu>
2019-01-29 19:44:11 -0500

    commit

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,7 @@
 * Email: kaurs4@students.rowan.edu
 ***********************************************
 * Purpose:
-*  This program calculates area of circle
+*  This program computes  varies distance and basic formulas for a cirlce.
 ***********************************************/
 
 #include <iostream>
@@ -20,9 +20,10 @@ int main()
 
 	float radius, area, circumference;
 	
+	
 	//Display purpose of the program
 	cout << " ******************************************"<<endl;
-	cout << " This program takes in radius to compute area of circle" <<endl;
+	cout << " This program takes in radius and circumference to compute area of circle" <<endl;
 	cout << " ******************************************\n\n";
 	
 	//Get user input
@@ -31,10 +32,54 @@ int main()
 	
 	//Compute area to PI*radius*radius
 	area = PI* radius *radius; 
+
+	//Compute circumference of circle
+	circumference = 2 * PI *radius;
 	
 	//Display the area 
 	cout << " The area is = " << area << "\n\n";
-	cout << "Program has ended"<<endl;
+	
+
+	//Display the circumference
+	cout << " The circumference is = " << circumference << "\n\n";
+	
+
+	float miles, kilometers, meters, millimeters, centimeters;
+
+
+	//Display purpose of the program
+	cout << " ******************************************" << endl;
+	cout << " This program converts kilometers to miles." << endl;
+	cout << " ******************************************\n\n";
+
+	//Get user input
+	cout << "Please input distance: ";
+	cin >> kilometers;
+
+	//Coversion from kilometers to miles
+	miles = 1 * kilometers / 1.6;
+	
+	//Display the coversion
+	cout << "The distance from kilometers to miles is: " << miles << "\n\n";
+
+
+	//Display purpose of the program
+	cout << " ******************************************" << endl;
+	cout << " This program converts meters to varies distances ." << endl;
+	cout << " ******************************************\n\n";
+
+	//Get user input 
+	cout << "Please input distance in meters: ";
+	cin >> meters;
+
+	//Coversion formulas
+	kilometers = meters *1000;
+	centimeters = meters / 100;
+	millimeters = meters / 1000;
+
+	//Display the conversion
+	cout << "The conversions are: " << kilometers << "km,  "<< centimeters << "cm, " << millimeters << "mm " << "\n\n";
+	cout << "Program has ended" << endl;
 
 	system("pause");
 	
